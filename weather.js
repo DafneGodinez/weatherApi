84c1e6ae73d5224dea8edbef215958994d641da6
DafneGodinez <A01732907@tec.mx>
2022-09-16 19:17:23 -0500

    Weather API v1

---
+++ weather.js
@@ -0,0 +1,43 @@
+
+var lat;
+var long;
+
+function WeatherAPI() {
+  var request = new XMLHttpRequest();
+  
+  // Open a new connection, using GET request on the URL endpoint
+  // Asincronas
+
+  request.onload = function (resp) {
+    console.log(resp.target.response);
+    console.log(JSON.parse(resp.target.response));
+    
+    var data = JSON.parse(resp.target.response);
+    console.log(data.name);
+    console.log(data.main.temp);
+    
+    var dataElement = document.getElementById("temp");
+    dataElement.innerHTML = "Temperatura: " + data.main.temp; 
+    
+    var dataElement2 = document.getElementById("city");
+    dataElement2.innerHTML = "Ciudad: " + data.name; 
+    
+    
+    
+  }
+  
+  var latElement = document.getElementById("Latitud");
+  var longElement = document.getElementById("Longitud");
+  lat = latElement.value;
+  long = longElement.value;
+  console.log(lat);
+  console.log(long);
+
+ request.open('GET', "https://api.openweathermap.org/data/2.5/weather?lat="+lat+"&lon="+long+"&appid=8e59d540bf55d8460dc988843b8c7cbb&units=metric", true);
+
+
+    request.send();
+     
+}
+
+
